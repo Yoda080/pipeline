bc6f4def06ee8b7ee16e1ebadda4e787987704da
walter <wdimauro@studenti.apuliadigitalmaker.it>
2025-09-11 15:18:35 +0200

    java

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,13 +1,10 @@
-
-
 window.salvaRisultatoInLocalStorage = (risultato) => {
-
     let risultati = localStorage.getItem('risultatiRiso');
     risultati = risultati ? JSON.parse(risultati) : [];
-    
-    
     risultati.push(risultato);
-
-   
     localStorage.setItem('risultatiRiso', JSON.stringify(risultati));
+};
+
+window.caricaRisultatiDaLocalStorage = () => {
+    return localStorage.getItem('risultatiRiso');
 };
